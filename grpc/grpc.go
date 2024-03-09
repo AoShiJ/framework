dc3980cd05b2176e5d7c6791f08cde446580a3a2
jsa <j1001574000@163.com>
2024-03-09 11:13:54 +0800

    1

--- grpc/grpc.go
+++ grpc/grpc.go
@@ -3,29 +3,28 @@ package grpc
 import (
 	"fmt"
 	"github.com/AoShiJ/framework/consul"
+	"github.com/astaxie/beego/logs"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/health"
 	"google.golang.org/grpc/health/grpc_health_v1"
 	"google.golang.org/grpc/reflection"
 	"log"
 	"net"
-	"strconv"
 )
 
-func RegisterGrpc(port, ip, serverName string, fc func(s *grpc.Server)) {
+func RegisterGrpc(IP, serverName string, port int, fc func(s *grpc.Server)) {
 
-	lis, err := net.Listen("tcp", fmt.Sprintf(":%s", port))
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
 	s := grpc.NewServer()
 	reflection.Register(s)
-	atoi, err := strconv.Atoi(port)
+	err = consul.RegisterConsul(serverName, IP, port)
 	if err != nil {
+		logs.Info(err, "------consul")
 		return
 	}
-
-	consul.RegisterConsul(int64(atoi), ip, serverName)
 	grpc_health_v1.RegisterHealthServer(s, health.NewServer())
 	fc(s)
 	log.Printf("server listening at %v", lis.Addr())
