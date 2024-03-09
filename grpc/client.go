58c013cc6a6b142139782f470579a067a0f42415
jsa <j1001574000@163.com>
2024-03-09 11:13:44 +0800

    1

--- grpc/client.go
+++ grpc/client.go
@@ -1,7 +1,6 @@
 package grpc
 
 import (
-	"context"
 	"fmt"
 	"github.com/AoShiJ/framework/consul"
 	"github.com/astaxie/beego/logs"
@@ -9,8 +8,8 @@ import (
 	"google.golang.org/grpc/credentials/insecure"
 )
 
-func Client(ctx context.Context, toService string) (*grpc.ClientConn, error) {
-	conn, err := consul.AgentHealthService(ctx, toService)
+func Client(toService, IP string, port int) (*grpc.ClientConn, error) {
+	conn, err := consul.AgentHealthService(toService, IP, port)
 	logs.Info(conn, 123)
 	logs.Info(toService, 321)
 	if err != nil {
